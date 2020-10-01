7ae40f02e5c68402534161dcde73b1d0267a5f11
Hector Dael <hector.jimenez@upr.edu>
2020-09-30 23:49:09 -0400

    Added Page and Components Structure. Ready to start developing.

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './Pages/landing-page/landing-page.component';
+import { LoginPageComponent } from './Pages/login-page/login-page.component';
+import { RegisterPageComponent } from './Pages/register-page/register-page.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
